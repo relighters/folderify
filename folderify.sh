0854c889c37b63a1b0ea6173d0a1449488821e4c
Relighters <git@rayoflight.cc>
2019-09-13 20:25:57 -0700

    Added "-d" flag for specifying sub-directory path

--- folderify.sh
+++ folderify.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 # Option flag parsing using bash's getopts function
-while getopts ":e:" option; do
+while getopts ":e:d:" option; do
     case "${option}" in
-        e) e=${OPTARG} ;;
+        e) e=${OPTARG} ;;       # enables "-e" flag for specifying file extension [dpx, exr, tiff, etc]
+        d) d=${OPTARG} ;;       # enables "-d" flag for specifying sub-directory path [i.e. resolution]
     esac
 done
 
 # Filename parsing, directory creation, and move operations
 for file in *."${e}"; do
            dir=${file%%.*}
-           mkdir -p -- "$dir"
-           mv "$file" "$dir"
+           mkdir -p -- "$dir/${d}"
+           mv "$file" "$dir/${d}"
 done
