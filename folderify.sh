e09312a61928d3f50840f7fb7f7ab28754239927
Relighters <git@rayoflight.cc>
2019-09-13 19:48:16 -0700

    Added "-e" flag for specifying target file extension using the bash "getops" function

--- folderify.sh
+++ folderify.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-for file in *.dpx; do
+
+# Option flag parsing using bash's getopts function
+while getopts ":e:" option; do
+    case "${option}" in
+        e) e=${OPTARG} ;;
+    esac
+done
+
+# Filename parsing, directory creation, and move operations
+for file in *."${e}"; do
            dir=${file%%.*}
            mkdir -p -- "$dir"
            mv "$file" "$dir"
